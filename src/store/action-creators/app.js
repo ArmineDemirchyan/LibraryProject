8be711eb9377466c0d803d1edacb3afdbc4c25c9
Eduard10110 <edgabrielyan777@gmail.com>
2022-04-13 10:48:10 +0400

    create user books-list page and reserve book container

--- src/store/action-creators/app.js
+++ src/store/action-creators/app.js
@@ -1,2 +1,11 @@
 import * as actionTypes from "../action-types/app";
 
+export const showBooksBasketContainer = (payload) => ({
+  type: actionTypes.SHOW_BOOKS_BASKET_CONTAINER,
+  payload,
+});
+
+export const BooksBasketListChange = (payload) => ({
+  type: actionTypes.BOOKS_BASKET_LIST_CHANGE,
+  payload,
+});
